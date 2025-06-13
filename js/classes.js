2ca486909da9bcf0cde8b17a3327c44321a5fb9f
Adriano Rocha <adrnrocha@gmail.com>
2025-06-12 22:23:26 -0300

    Update classes.js

--- js/classes.js
+++ js/classes.js
@@ -1,1 +1,30 @@
+export class Cliente {
+  constructor(nome, email) {
+    this.nome = nome;
+    this.email = email;
+  }
+}
 
+export class ClienteService {
+  constructor(apiBase) {
+    this.apiBase = apiBase;
+  }
+
+  async cadastrar(cliente) {
+    const resposta = await fetch(this.apiBase, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(cliente)
+    });
+    return resposta.json();
+  }
+
+  async listarTodos() {
+    const resposta = await fetch(this.apiBase);
+    return resposta.json();
+  }
+
+  async excluir(id) {
+    await fetch(`${this.apiBase}/${id}`, { method: "DELETE" });
+  }
+}
