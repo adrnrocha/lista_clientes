7919914ade21c52861e529bf01071d4783e4556e
Adriano Rocha <adrnrocha@gmail.com>
2025-06-12 22:23:43 -0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,13 @@
+export function criarElementoCliente(cliente, aoExcluir) {
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <strong>${cliente.nome}</strong> - ${cliente.email}
+    <button class="btn-excluir">Excluir</button>
+  `;
+  li.querySelector("button").addEventListener("click", () => aoExcluir(cliente._id));
+  return li;
+}
 
+export function limparFormulario(form) {
+  form.reset();
+}
